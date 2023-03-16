db7952ddc97dfaaee07ca7c865fbcf4e94667d0f
miltonsito <109830559+miltonsito@users.noreply.github.com>
2023-03-16 15:35:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,28 @@
 from fastapi import FastAPI
 import pandas as pd
 
+app = FastAPI(title="Películas y Series")
 
-app = FastAPI( title="Películas y Series")
-
-@app.get ("/")
+@app.get("/")
 async def read_root():
     return {"******* Hola, Mi nombre es Milton!! Este es mi Proyecto Nº 1 para Henry. ******"}
 
-
-#Consultas
-
-'''
-Película con mayor duración con filtros opcionales de AÑO, PLATAFORMA Y TIPO DE DURACIÓN. 
-(la función debe llamarse get_max_duration(year, platform, duration_type))
-'''
 # Película con mayor duración con filtros opcionales de AÑO, PLATAFORMA Y TIPO DE DURACIÓN
 @app.get("/get_max_duration")
 def get_max_duration(year: int, platform: str, duration_type: str):
-    movie_df=pd.read_csv('pelis_score.or.csv')
+    movie_df = pd.read_csv('pelis_score.or.csv')
     # Filtrar los datos según los parámetros recibidos
-    filtered_df = movie_df.copy()
+    filtered_df = movie_df
     if year:
-        filtered_df = filtered_df[filtered_df.Year["year"] == year]
+        filtered_df = filtered_df[filtered_df.Year == year]
     if platform:
-        filtered_df = filtered_df[filtered_df.Platform["plataform"] == platform]
+        filtered_df = filtered_df[filtered_df.Platform == platform]
     if duration_type:
-        filtered_df = filtered_df[filtered_df.Duration["duration_type"] == duration_type]
+        filtered_df = filtered_df[filtered_df.Duration == duration_type]
 
     # Encontrar la película con mayor duración en el dataframe filtrado
-    max_duration = filtered_movies['duration_int'].max()
-    max_duration_movie = filtered_movies[filtered_movies['duration_int'] == max_duration].iloc[0]
+    max_duration = filtered_df['duration_int'].max()
+    max_duration_movie = filtered_df[filtered_df['duration_int'] == max_duration].iloc[0]
 
     # Devolver el título de la película con mayor duración y su duración
     return {"title": max_duration_movie["title"], "duration_int": max_duration_movie["duration_int"]}
