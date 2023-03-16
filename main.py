7fe44dffc74589467637dc26518cbbf14b98c480
miltonsito <109830559+miltonsito@users.noreply.github.com>
2023-03-16 12:49:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,30 +6,31 @@ app = FastAPI( title="Películas y Series")
 
 @app.get ("/")
 async def read_root():
-    return {"******* Hola, Mi nombre es Milton!! Este es mi Proyecto Nª 1 para Henry. ******"}
+    return {"******* Hola, Mi nombre es Milton!! Este es mi Proyecto Nº 1 para Henry. ******"}
 
 
 #Consultas
 
 '''
-1. Cantidad de veces que aparece una keyword en el título de
- peliculas/series, por plataforma.
+Película con mayor duración con filtros opcionales de AÑO, PLATAFORMA Y TIPO DE DURACIÓN. 
+(la función debe llamarse get_max_duration(year, platform, duration_type))
 '''
-@app.get("/get_word_count")
-def get_word_count(plataforma: str, keyword: str):
-    movie_df=pd.read_csv('pelis_score')
-    #pasamos las entradas a minúsculas
-    plataforma =plataforma.lower()
-    keyword=keyword.lower()
-    #creamos la columna 'platform' 
-    movie_df['platform']=movie_df['id'].str[0]
-    #filtramos por plataforma
-    platform_df = movie_df[movie_df['platform'] == plataforma[0]]
-    #extramos los titulos en dicha plataforma 
-    titles = platform_df['title']
-    #luego iteramos a través de los títulos de las filas filtradas
-    count = 0
-    for title in titles:
-        count += title.count(keyword)
-    return f"La keyword '{keyword}' aparece {count} veces en los títulos de películas y series de la plataforma {plataforma}."
+# Película con mayor duración con filtros opcionales de AÑO, PLATAFORMA Y TIPO DE DURACIÓN
+@app.get("/get_max_duration")
+def get_max_duration(year: int, platform: str, duration_type: str):
+    movie_df=pd.read_csv('pelis_score1.csv')
+    # Filtrar los datos según los parámetros recibidos
+    filtered_df = movie_df.copy()
+    if year:
+        filtered_df = filtered_df[filtered_df.Year == year]
+    if platform:
+        filtered_df = filtered_df[filtered_df.Platform == platform]
+    if duration_type:
+        filtered_df = filtered_df[filtered_df.Duration == duration_type]
+
+    # Encontrar la película con mayor duración en el dataframe filtrado
+    max_duration_movie = filtered_df[filtered_df.Duration == filtered_df.Duration.max()].iloc[0]
+
+    # Devolver el título de la película con mayor duración y su duración
+    return {"title": max_duration_movie.Title, "duration": max_duration_movie.Duration}
 
